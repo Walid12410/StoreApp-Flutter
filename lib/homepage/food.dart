b9a5b5c5ab1450b0a37b0f71578c03e1f1668403
walid jallad <78869109+Walid12410@users.noreply.github.com>
2024-01-15 22:53:55 +0200

    Update food.dart

--- lib/homepage/food.dart
+++ lib/homepage/food.dart
@@ -65,7 +65,7 @@ class _FoodState extends State<Food> {
 
   Future<List<Item>> DrinksItemsFromApi() async {
     final response = await http.get(
-        Uri.parse('https://webhostwebhost186.000webhostapp.com/getItems.php'));
+        Uri.parse('use json file link/getItems.php'));
 
     if (response.statusCode == 200) {
       List<dynamic> jsonList = json.decode(response.body);
